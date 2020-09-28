dec90e80c935ab60144ff5e762a725690f77c44f
dmitri-dev <60167177+dmitri-dev@users.noreply.github.com>
2020-09-28 08:40:18 -0400

    Test Version

--- MainPageTest.java
+++ MainPageTest.java
@@ -3,11 +3,16 @@ package com.example.instagram_test;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 import java.util.Random;
 
 public class MainPageTest {
+    public WebDriver driver = setDriver();
+    public Actions actions = new Actions(driver);
+    public Random rand = new Random();
+    public int setAttempts = 3;
     public int followRate = 10;
     public String username = "sudokevin";
     public String password = "jhunter";
@@ -15,20 +20,49 @@ public class MainPageTest {
         System.setProperty("webdriver.chrome.driver", "C:/Programs/chromedriver_win32/chromedriver.exe");
         return new ChromeDriver();
     }
-    public List<WebElement> getPosts(WebDriver driver, int i) throws InterruptedException {
+    public List<WebElement> getPosts(int attempts) throws InterruptedException {
         Thread.sleep(1000);
         List<WebElement> posts = driver.findElements(By.cssSelector("div._9AhH0"));
         if (!posts.isEmpty()) {
             return posts;
-        } else if (i < 3) {
-            getPosts(driver, i + 1);
+        } else if (attempts > 0) {
+            getPosts(--attempts);
         }
         return posts;
+    }
+    public void getPost(List<WebElement> posts) throws InterruptedException {
+        Thread.sleep(1000);
+        int random = rand.nextInt((posts.size()) - 1);
+        posts.get(random).click();
+        Thread.sleep(1000);
+        boolean isPhoto = !driver.findElements(By.cssSelector("button.sqdOP.yWX7d._8A5w5")).isEmpty();
+        if (!isPhoto) {
+            System.out.println("Invalid Post");
+            actions.sendKeys(Keys.ESCAPE).build().perform();
+            getPost(posts);
+        }
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("button.sqdOP.yWX7d._8A5w5")).click();
+    }
+    public void getFollowers(List<WebElement> people, int attempts) throws InterruptedException {
+        Thread.sleep(1000);
+        if (people.size() > followRate) {
+            follow(people, followRate);
+        } else if (attempts > 0) {
+            driver.navigate().refresh();
+            getFollowers(people, --attempts);
+        }
+    }
+    public void follow(List<WebElement> people, int followRate) throws InterruptedException {
+        Thread.sleep(1000);
+        if (followRate > 0) {
+            people.get(people.size() - followRate).click();
+            follow(people, --followRate);
+        }
     }
 
     @Test
     public void openPage() throws InterruptedException {
-        WebDriver driver = setDriver();
         driver.get("https://www.instagram.com/");
 
         // LOGIN
@@ -38,58 +72,42 @@ public class MainPageTest {
         Thread.sleep(1000);
         List<WebElement> buttons = driver.findElements(By.tagName("button"));
         buttons.get(1).click();
+        Thread.sleep(1000);
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("button.sqdOP.L3NKy.y3zKF")).click();
         Thread.sleep(2000);
         driver.findElement(By.tagName("button")).click();
-        Thread.sleep(1000);
-        buttons = driver.findElements(By.xpath("//button[@tabindex='0']"));
-        buttons.get(1).click();
 
-        // EXPLORE PAGE
-        Thread.sleep(1000);
-        List<WebElement> icons = driver.findElements(By.cssSelector("svg._8-yf5"));
-        icons.get(2).click();
-        Thread.sleep(1000);
-        //-----------------------//
-        /*
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
-        Thread.sleep(1000);
-        */
-        //-----------------------//
-        List<WebElement> posts = getPosts(driver, 0);
-        //-----------------------//
-//        System.out.println(posts);
-        //-----------------------//
-        Random rand = new Random();
-        int random = rand.nextInt((posts.size()) - 1);
-        posts.get(random).click();
-
-        // POST (IMAGE / VIDEO)
-        Thread.sleep(2000);
-        boolean photo = true;
-        try {
-            driver.findElement(By.cssSelector("button.sqdOP.yWX7d._8A5w5")).click();
-        } catch (Exception e) {
-            System.out.println(e + "\n" + "Like Button Not Found");
-            photo = false;
-        }
-        if (!photo) {
-            System.out.println("Not Photo");
-            driver.findElement(By.cssSelector(""));
-        }
-
-        // Likes
-        List<WebElement> people = driver.findElements(By.cssSelector("button.sqdOP.L3NKy.y3zKF"));
-        if (!people.isEmpty()) {
-            if (followRate > people.size()) {
-                for (int i = 0; i <= people.size(); i++) {
-                    people.get(i).click();
-                }
-            } else {
-                for (int i = 0; i <= followRate; i++) {
-                    people.get(i).click();
-                }
-            }
-        }
+//        buttons = driver.findElements(By.xpath("//button[@tabindex='0']"));
+//        buttons.get(1).click();
+//
+//        // EXPLORE PAGE
+//        Thread.sleep(1000);
+//        List<WebElement> icons = driver.findElements(By.cssSelector("svg._8-yf5"));
+//        icons.get(2).click();
+//        Thread.sleep(1000);
+//        //-----------------------//
+//        /*
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+//        Thread.sleep(1000);
+//        */
+//        //-----------------------//
+//        List<WebElement> posts = getPosts(setAttempts);
+//        //-----------------------//
+////        System.out.println(posts);
+//        //-----------------------//
+//
+//        // POST
+//        getPost(posts);
+//
+//        // LIKES
+//        Thread.sleep(1000);
+//        List<WebElement> people = driver.findElements(By.cssSelector("button.sqdOP.L3NKy.y3zKF"));
+//        getFollowers(people, setAttempts);
+//        for (WebElement person : people) {
+//            System.out.println(person);
+//        }
     }
 }
